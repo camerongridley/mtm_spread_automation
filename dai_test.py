6dba0e332e332df38f06c10bd7d39044d1d928e6
Chandrakumar K <chandrakumar3108@gmail.com>
2021-07-02 00:37:33 +0530

    Updated code for populating DAI metrics in the template sheet. Fixed column rearrangement according to dynamodb json response.

--- dai_test.py
+++ dai_test.py
@@ -1,112 +1,93 @@
-import openpyxl
-import json
-import pandas as pd
 from openpyxl import load_workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
+
+dynamodb_response = [
+    {
+        "Metrics": "Completed Impression Rate (%)",
+        "Last Full Week": "95.67",
+        "Date": "2021-06-30",
+        "Prior Full Week": "96.56",
+        "Target / Benchmark": "90",
+        "MTD": "97.93",
+        "This Quarter": "98.18"
+    },
+    {
+        "Metrics": "Fill rate | Ad only (%)",
+        "Last Full Week": "82.15",
+        "Date": "2021-06-30",
+        "Prior Full Week": "85.12",
+        "Target / Benchmark": "-",
+        "MTD": "82.7",
+        "This Quarter": "81.12"
+    },
+    {
+        "Metrics": "Fill rate | Ad+Promo (%)",
+        "Last Full Week": "90.56",
+        "Date": "2021-06-30",
+        "Prior Full Week": "91.45",
+        "Target / Benchmark": "100",
+        "MTD": "92.56",
+        "This Quarter": "93.05"
+    },
+    {
+        "Metrics": "Uses Impression Rate (%)",
+        "Last Full Week": "63.51",
+        "Date": "2021-06-30",
+        "Prior Full Week": "66.48",
+        "Target / Benchmark": "60",
+        "MTD": "66.51",
+        "This Quarter": "67.16"
+    }
+]
+
 
 class ExcelAutomator(object):
-    def __init__(self, filename) -> None:
-        self.filename = filename
-        self.wb = load_workbook(filename)
+    # Class Instantiation
+    def __init__(self, file_name) -> None:
+        self.file_name = file_name
+        self.wb = load_workbook(file_name)
 
+    # Get sheet from the workbook
     def get_sheet(self, sheet_name):
         ws = self.wb[sheet_name]
         return ws
-    
-    def updated_dai_df(self, ws, df:pd.DataFrame):
-        d = df.to_dict(orient='index')
 
-        for cell, data in d.items():
-            ws[cell]=data['value']
+    # DAI metrics updating in excel template file
+    def updated_dai(self, ws, response) -> None:
+        # Required Column Headers
+        headers = {col_name.value: col_name.column_letter for col_name in list(ws.rows)[0][1:11]}
 
-    def updated_dai(self, ws, metrics:json):
+        # Metrics search and header column letter rearrangement
+        cell = []
+        for each in response:
+            row = self.search_value_in_col_idx(ws, each['Metrics'])
+            cell.append([(f"{headers[key]}{row}", key) for key in response[0].keys() if key not in ['Date', 'Metrics']])
 
-        for metric in d['metrics']:
-            cell = metric['this_week_cell']
-            ws[cell]=metric['this_week']
+        # Assign cell values to respective cells for all DAI metrics
+        for each_row, data in zip(cell, response):
+            ws[each_row[0][0]] = data[each_row[0][1]]
+            ws[each_row[1][0]] = data[each_row[1][1]]
+            ws[each_row[2][0]] = data[each_row[2][1]]
+            ws[each_row[3][0]] = data[each_row[3][1]]
+            ws[each_row[4][0]] = data[each_row[4][1]]
 
-            cell = self.get_next_col(cell)
-            ws[cell] = metric['last_week']
+    # Search row number of each metrics for cell value generation
+    @staticmethod
+    def search_value_in_col_idx(ws, metrics_name, col_idx=1):
+        for row in range(1, ws.max_row + 1):
+            if ws[row][col_idx].value == metrics_name:
+                return row
+        print(f'{metrics_name} not found!!')
 
-            cell = self.get_next_col(cell)
-            ws[cell] = metric['MTD']
-
-            cell = self.get_next_col(cell)
-            ws[cell] = metric['QTD']
-
-            cell = self.get_next_col(cell)
-            ws[cell] = metric['target']
-
-
-    def move_to_last_week(self, ws, df:pd.DataFrame):
-        ws.move_range()
-
-    def save(self, filename):
-        self.wb.save(filename=filename)
-
-    def get_next_col(self, cell_loc:str)->str:
-        #split col from row
-        col = ''
-        row = ''
-
-        for c in cell_loc:
-            if c.isnumeric():
-                row = row + c
-            elif c.isalpha():
-                col = col + c
-            else:
-                print('Cell location is invalid.')
-
-        next_col = chr(ord(col)+1)
-
-        return next_col + row
+    # Save the changes in the workbook
+    def save(self, file_name):
+        self.wb.save(filename=file_name)
 
 
+# Main Constructor
 if __name__ == "__main__":
-    cols = ['date', 'metric', 'value', 'cell']
-    data = [['1-1-21', 'Uses Impression Rate (%)', 1.0, 'D33'], 
-            ['1-1-21', 'Fill rate, Ad only (%)', 2.3, 'D34'], 
-            ['1-1-21', 'Fill rate, Ad+Promo (%)', 3.6, 'D35'], 
-            ['1-1-21', 'Completed Impression Rate (%)', 4.9, 'D36']
-        ]
+    filename = 'Team Metrics.xlsx'
+    excel_automator = ExcelAutomator(filename)
+    work_sheet = excel_automator.get_sheet('SlingTV')
+    excel_automator.updated_dai(work_sheet, dynamodb_response)
+    excel_automator.save(filename)
 
-    d = {'metrics':[
-        {'date' : '1-1-21', 'name' : 'Uses Impression Rate (%)', 'this_week' : 65.89, 'last_week' : 66.51, 'MTD' : 66.35, 'QTD' : 67.06, 'target' : 12, 'this_week_cell' : 'D33'},
-        {'date' : '1-1-21', 'name' : 'Fill rate, Ad only (%)', 'this_week' : 1.89, 'last_week' : 11.51, 'MTD' : 111.35, 'QTD' : 1111.06, 'target' : 23, 'this_week_cell' : 'D34'},
-        {'date' : '1-1-21', 'name' : 'Fill rate, Ad+Promo (%)', 'this_week' : 2.89, 'last_week' : 22.51, 'MTD' : 222.35, 'QTD' : 2222.06, 'target' : 34, 'this_week_cell' : 'D35'},
-        {'date' : '1-1-21', 'name' : 'ompleted Impression Rate (%)', 'this_week' : 3.89, 'last_week' : 33.51, 'MTD' : 333.35, 'QTD' : 3333.06, 'target' : 45, 'this_week_cell' : 'D36'}
-    ]}
-
-
-    metric_json = json.loads(json.dumps(d))
-
-    df = pd.DataFrame(columns=cols, data=data)
-    df.index = df['cell']
-    df.drop('cell', axis=1)
-    filename = 'TeamMetrics.xlsx'
-
-    exa = ExcelAutomator(filename)
-    ws = exa.get_sheet('SlingTV')
-    exa.updated_dai(ws, metric_json)
-    exa.save(filename)
-
-    #print(exa.get_next_col('D33'))
-'''
-,
-            ['1-8-21', 'Uses Impression Rate (%)', 1.1, 'D33'], 
-            ['1-8-21', 'Fill rate, Ad only (%)', 2.4, 'D34'], 
-            ['1-8-21', 'Fill rate, Ad+Promo (%)', 3.7, 'D35'], 
-            ['1-8-21', 'Completed Impression Rate (%)', 5.0, 'D36'],
-            ['1-15-21', 'Uses Impression Rate (%)', 0.9, 'D33'], 
-            ['1-15-21', 'Fill rate, Ad only (%)', 2.2, 'D34'], 
-            ['1-15-21', 'Fill rate, Ad+Promo (%)', 3.5, 'D35'], 
-            ['1-15-21', 'Completed Impression Rate (%)', 4.8, 'D36'],
-            ['1-22-21', 'Uses Impression Rate (%)', 1.0, 'D33'], 
-            ['1-22-21', 'Fill rate, Ad only (%)', 2.3, 'D34'], 
-            ['1-22-21', 'Fill rate, Ad+Promo (%)', 3.6, 'D35'], 
-            ['1-22-21', 'Completed Impression Rate (%)', 4.9, 'D36'],
-            ['1-30-21', 'Uses Impression Rate (%)', 1.3, 'D33'], 
-            ['1-30-21', 'Fill rate, Ad only (%)', 2.6, 'D34'], 
-            ['1-30-21', 'Fill rate, Ad+Promo (%)', 3.9, 'D35'], 
-            ['1-30-21', 'Completed Impression Rate (%)', 5.2, 'D36'],
-'''
